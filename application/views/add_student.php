47c6b49b758881b27598163ad6cb35c8e6454d4a
newlls <523786175@qq.com>
2018-11-02 16:47:25 +0800

    后台管理

---
+++ application/views/add_student.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>add</title>
+</head>
+<body>
+<form action="add" method="post">
+    <table border='1'>
+        <tr>
+            <td>学生学号</td>
+            <td><input type="text" name="sNo" id="sNo"/></td>
+        </tr>
+        <tr>
+            <td>学生姓名</td>
+            <td><input type="text" name="sName" id="sName"/></td>
+        </tr>
+        <tr>
+            <td>年龄</td>
+            <td><input type="text" name="sAge" id="sAge"/></td>
+        </tr>
+        <tr>
+            <td>性别</td>
+            <td><input type="text" name="sex" id="sex"/></td>
+        </tr>
+
+        <tr>
+            <td>班级</td>
+            <td><input type="text" name="sClass" id="sClass"/></td>
+        </tr>
+        <tr>
+            <td>学校</td>
+            <td><input type="text" name="school" id="school"/></td>
+        </tr>
+        <tr>
+            <td>专业</td>
+            <td><input type="text"  name="major" id="major"/></td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td><input type="submit" class="button" name="OK" value="提交" />
+            </td>
+        </tr>
+    </table></form>
+
+</body>
+</html>
+
+
